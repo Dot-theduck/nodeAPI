9312b48d8878f2e5bf18096b13d40d666d8aea16
Emmanuel Kiptoo <118376335+Dot-theduck@users.noreply.github.com>
2024-02-29 21:56:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 //routes
 
-app.get('/', (req,res) => {
-    res.send('Hello Node API')
-})
-app.get('/blog', (req,res) => {
-    res.send('Hello Blog blaaah')
-})
 
-app.get('/products', async(req,res) => {
+
+app.get('/', async(req,res) => {
     try {
         const products = await Product.find({});
         res.status(200).json(products)
@@ -24,7 +19,7 @@ app.get('/products', async(req,res) => {
     }
 })
 
-app.get('/products/:id', async(req, res) =>{
+app.get('/:id', async(req, res) =>{
     try {
         const {id} = req.params;
         const product = await Product.findById(id);
@@ -35,7 +30,7 @@ app.get('/products/:id', async(req, res) =>{
 })
 
 //update a product
-app.put('/products/:id', async(req,res) =>{
+app.put('/:id', async(req,res) =>{
     try {
         const {id} = req.params;
         const product = await Product.findByIdAndUpdate(id, req.body);
